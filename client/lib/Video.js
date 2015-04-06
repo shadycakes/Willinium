f2c709613ce28f8291dfa81488296819db3c4975
Trevor J McDonald <trevor.j.mcdonald.personal@gmail.com>
2015-04-05 21:35:39 -0400

    Quick submit check

--- client/lib/Video.js
+++ client/lib/Video.js
@@ -8,6 +8,36 @@ Template.tmpCreateVideo.helpers({
 
 Template.tmpCreateVideo.events({
    'click .btn-primary': function () {
-       console.log(this);
+       // SUBMIT
+       console.log('tmpCreateVideo - Submit');
+
+       var tmpCreateVideo = $('.tmpCreateVideo');
+
+       if (!tmpCreateVideo) {
+           console.log(tmpCreateVideo);
+           return false;
+       }
+
+       // TODO: Validation
+       var validate = true;
+
+       if (!validate) {
+           // TODO: Inform user of mistakes
+           return false;
+       }
+
+       var newUserVideo = {
+           creator: Meteor.user().id,
+           title: tmpCreateVideo.find('input[name="title"]').val(),
+           logline: tmpCreateVideo.find('input[name="logline"]').val(),
+           synopsis: tmpCreateVideo.find('input[name="synopsis"]').val(),
+           vimeolink: tmpCreateVideo.find('input[name="vimeolink"]').val()
+       };
+
+       console.log(newUserVideo);
+
+       /*userVideos.insert({
+
+       });*/
    }
 });
---
+++ collections/userVideos.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Trevor on 4/5/2015.
+ */
+
+userVideos = new Mongo.Collection("userVideos");
