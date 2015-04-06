3dd50c4c7b0700db4ad305c5560f13e274970303
Trevor J McDonald <trevor.j.mcdonald.personal@gmail.com>
2015-04-05 22:16:25 -0400

    List test

--- client/lib/Video.js
+++ client/lib/Video.js
@@ -27,10 +27,10 @@ Template.tmpCreateVideo.events({
        }
 
        console.log('creator: ' + Meteor.user().username);
-       console.log('title: ' + tmpCreateVideo.children('input[name="title"]').val());
-       console.log('logline: ' + tmpCreateVideo.children('input[name="logline"]').val());
-       console.log('synopsis: ' + tmpCreateVideo.children('textarea[name="synopsis"]').val());
-       console.log('vimeolink: ' + tmpCreateVideo.children('input[name="vimeolink"]').val());
+       console.log('title: ' + tmpCreateVideo.find('input[name="title"]').val());
+       console.log('logline: ' + tmpCreateVideo.find('input[name="logline"]').val());
+       console.log('synopsis: ' + tmpCreateVideo.find('textarea[name="synopsis"]').val());
+       console.log('vimeolink: ' + tmpCreateVideo.find('input[name="vimeolink"]').val());
 
        var newUserVideo = {
            owner: Meteor.userId(),
@@ -43,8 +43,19 @@ Template.tmpCreateVideo.events({
 
        console.log(newUserVideo);
 
-       /*userVideos.insert({
-
-       });*/
+       userVideos.insert({
+           owner: Meteor.userId(),
+           creator: Meteor.user().username,
+           title: tmpCreateVideo.find('input[name="title"]').val(),
+           logline: tmpCreateVideo.find('input[name="logline"]').val(),
+           synopsis: tmpCreateVideo.find('textarea[name="synopsis"]').val(),
+           vimeolink: tmpCreateVideo.find('input[name="vimeolink"]').val()
+       });
    }
+});
+
+Template.tmpSearchVideo.helpers({
+    getUserVideos: function () {
+        return userVideos.find({});
+    }
 });
