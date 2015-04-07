85c883f5e3f59df6e93f36f483278172d1e83197
Trevor J McDonald <trevor.j.mcdonald.personal@gmail.com>
2015-04-07 19:30:10 -0400

    More routing

--- client/router.js
+++ client/router.js
@@ -1,10 +1,60 @@
 Router.configure({
-    layoutTemplate: 'layout'
+    layoutTemplate: 'template-layout',
+    loadingTemplate: 'template-loading',
+    notFoundTemplate: 'template-not-found',
+    load: function () {
+        $('html, body').animate({ scrollTop: 0 }, 400)
+        $('.main-content').hide().fadeIn(1000)
+    }
 });
 
 Router.map(function () {
     this.route('home', {
         path: '/',
-        template: 'home'
+        template: 'home',
+        onBeforeAction: function () {
+            // TODO: Implement https://atmospherejs.com/manuelschoebel/ms-seo
+            return document.title = "Diane Edulizer - Education for the Future";
+        }
+    });
+    this.route('searchVideo', {
+        path: '/video/search/:searchterms?',
+        template: 'template-search-video',
+        onBeforeAction: function () {
+            // TODO: Implement https://atmospherejs.com/manuelschoebel/ms-seo
+            return document.title = "Diane Edulizer - Searching videos";
+        }
+    });
+    this.route('postVideo', {
+        path: '/video/post',
+        template: 'template-post-video',
+        onBeforeAction: function () {
+            // TODO: Implement https://atmospherejs.com/manuelschoebel/ms-seo
+            return document.title = "Diane Edulizer - Post a video";
+        }
+    });
+    this.route('editVideo', {
+        path: '/video/edit/:_id',
+        template: 'template-edit-video',
+        data: function () {
+            return userVideos.findOne({_id: this.params._id});
+        },
+        onBeforeAction: function () {
+            // TODO: Implement https://atmospherejs.com/manuelschoebel/ms-seo
+            var videoTitle = this.data().title;
+            return document.title = "Diane Edulizer - Editing '" + videoTitle + "'";
+        }
+    });
+    this.route('watchVideo', {
+        path: '/video/watch/:_id',
+        template: 'template-watch-video',
+        data: function () {
+            return userVideos.findOne({_id: this.params._id});
+        },
+        onBeforeAction: function () {
+            // TODO: Implement https://atmospherejs.com/manuelschoebel/ms-seo
+            var videoTitle = this.data().title;
+            return document.title = "Diane Edulizer - " + videoTitle;
+        }
     });
 });
